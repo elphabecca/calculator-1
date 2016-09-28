5567e3b075cbc2294f00eb34a94934dcb307561e
Hackbright Student <no-reply@hackbrightacademy.com>
2016-09-28 12:38:47 -0700

    code review approved version

--- arithmetic.py
+++ arithmetic.py
@@ -2,13 +2,13 @@ def add(num1, num2):
     return num1 + num2
 
 def subtract(num1, num2):
-    return num1 - num2
+    return (num1 - num2)
 
 def multiply(num1, num2):
     return num1 * num2
 
 def divide(num1, num2):
-    return num1 / num2
+    return num1 / float(num2)
 
 def square(num1):
     return num1 ** 2
